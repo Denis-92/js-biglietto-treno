3b3e4d8d17ad77345e251a5b033ba0efa25f0cf2
Denis-92 <d.mozenic@gmail.com>
2022-08-23 17:35:23 +0200

    (NON FUNZIONA) aggiunto controllo campo vuoto

--- javascript/script.js
+++ javascript/script.js
@@ -8,27 +8,36 @@ const scontoSenior = 0.6;
 let costoBiglietto = distanzaDaPercorrere * prezzo;
 let prezzoFinale = distanzaDaPercorrere * prezzo;
 
-if (isNaN(distanzaDaPercorrere) || isNaN(etaPasseggero)) {
+if ((isNaN(distanzaDaPercorrere)) || (isNaN(etaPasseggero))) {
     document.getElementById('prezzoAcquisto').innerHTML = `Dati inseriti non validi`;
 }
 else {
-    if (etaPasseggero < 18) {
-        console.log('sconto minori', scontoMinori);
-        console.log('costo biglietto', costoBiglietto);
-        prezzoFinale = costoBiglietto * scontoMinori;
+    if ((distanzaDaPercorrere === 0) || (etaPasseggero === 0)) {
+        console.log('ciao visualizza', scontoMinori);
+        document.getElementById('prezzoAcquisto').innerHTML = `Dati mancanti`;
     }
 
     else {
 
-        if (etaPasseggero > 65) {
-            prezzoFinale = costoBiglietto * scontoSenior;
+
+        if (etaPasseggero < 18) {
+            console.log('sconto minori', scontoMinori);
+            console.log('costo biglietto', costoBiglietto);
+            prezzoFinale = costoBiglietto * scontoMinori;
         }
 
+        else {
+
+            if (etaPasseggero > 65) {
+                prezzoFinale = costoBiglietto * scontoSenior;
+            }
+
+        }
+
+        console.log('prezzo finale prima del output', prezzoFinale);
+
+        document.getElementById('prezzoAcquisto').innerHTML = `Il prezzo del tuo biglietto è ${prezzoFinale}`;
     }
-
-    console.log('prezzo finale prima del output', prezzoFinale);
-
-    document.getElementById('prezzoAcquisto').innerHTML = `Il prezzo del tuo biglietto è ${prezzoFinale}`;
 }
 
 
