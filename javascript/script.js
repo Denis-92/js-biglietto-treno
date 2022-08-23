5f500e6a750b311db88edc5d8b39f5dc2327cf13
Denis-92 <d.mozenic@gmail.com>
2022-08-23 16:06:09 +0200

    aggiunto controllo eta' per sconto

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,15 @@ const scontoMinori = 0.8;
 const scontoSenior = 0.6;
 
 let costoBiglietto = distanzaDaPercorrere * prezzo;
+
+if (etaPasseggiero < 18) {
+    let prezzoFinale = costoBiglietto * scontoMinori;
+}
+
+else {
+
+    if (etaPasseggiero > 65) {
+        let prezzoFinale = costoBiglietto * scontoSenior;
+    }
+
+}
