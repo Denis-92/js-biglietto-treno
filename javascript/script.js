4e977bfa6ee188ada3d523794320f57224b57c0a
Denis-92 <d.mozenic@gmail.com>
2022-08-23 16:35:40 +0200

    aggiunto output

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-let distanzaDaPercorrere = prompt('Quanti chilometri devi percorrere?');
-let etaPasseggero = prompt('Eta\' del passeggero?');
+let distanzaDaPercorrere = prompt('Quanti chilometri da percorrere?');
+let etaPasseggero = prompt('Età del passeggero?');
 
 const prezzo = 0.21;
 const scontoMinori = 0.8;
 const scontoSenior = 0.6;
 
-let costoBiglietto = distanzaDaPercorrere * prezzo;
+let costoBiglietto, prezzoFinale = distanzaDaPercorrere * prezzo;
 
 if (etaPasseggero < 18) {
     let prezzoFinale = costoBiglietto * scontoMinori;
@@ -18,3 +18,8 @@ else {
     }
 
 }
+
+// document.getElementById('prezzoAcquisto').innerHTML = 'Il prezzo del tuo biglietto è prezzoFinale';
+
+document.writeln('Il prezzo del tuo biglietto è');
+document.writeln(prezzoFinale);
